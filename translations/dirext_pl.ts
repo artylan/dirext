34a9c33af952d130c07628c8d06201f06c1462ca
Artur Wystub <artur.wystub@arcor.de>
2020-10-27 16:19:43 +0100

    add polish translation

---
+++ translations/dirext_pl.ts
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl_PL">
+<context>
+    <name>CustomTableModel</name>
+    <message>
+        <location filename="../customtablemodel.cpp" line="60"/>
+        <source>Extension</source>
+        <translation>Rozszerzenie pliku</translation>
+    </message>
+    <message>
+        <location filename="../customtablemodel.cpp" line="63"/>
+        <source>Number of files</source>
+        <translation>Liczba plików</translation>
+    </message>
+    <message>
+        <location filename="../customtablemodel.cpp" line="66"/>
+        <source>Size in Bytes</source>
+        <translation>Wielkość w bajtach</translation>
+    </message>
+</context>
+<context>
+    <name>ProgressDialog</name>
+    <message>
+        <location filename="../progressdialog.ui" line="14"/>
+        <source>DirExt</source>
+        <translation>DirExt</translation>
+    </message>
+    <message>
+        <location filename="../progressdialog.ui" line="62"/>
+        <source>Cancel</source>
+        <translation>Anuluj</translation>
+    </message>
+    <message>
+        <location filename="../progressdialog.cpp" line="21"/>
+        <source>Number of inspected files:</source>
+        <translation>Liczba zbadanych plików:</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../main.cpp" line="20"/>
+        <source>Inspect directory</source>
+        <translation>Sprawdź folder</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="25"/>
+        <source>You haven&apos;t specified a directory.</source>
+        <translation>Nie podałeś foldera.</translation>
+    </message>
+    <message>
+        <location filename="../main.cpp" line="33"/>
+        <source>doesn&apos;t exist or isn&apos;t a directory.</source>
+        <translation>nie istnieje lub nie jest folderem.</translation>
+    </message>
+</context>
+<context>
+    <name>Widget</name>
+    <message>
+        <location filename="../widget.ui" line="14"/>
+        <source>DirExt</source>
+        <translation>DirExt</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="25"/>
+        <source>Directory:</source>
+        <translation>Folder:</translation>
+    </message>
+    <message>
+        <location filename="../widget.cpp" line="26"/>
+        <source>Number of inspected files:</source>
+        <translation>Liczba zbadanych plików:</translation>
+    </message>
+</context>
+</TS>
